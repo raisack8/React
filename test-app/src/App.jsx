d111c2113627f8a5b11e680502b7cff716a5573d
raisack8 <raisack8@gmail.com>
2023-06-22 23:43:32 +0900

    testcommit

--- test-app/src/App.jsx
+++ test-app/src/App.jsx
@@ -38,7 +38,7 @@ function getPosts() {
       <button class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
       onClick={getPosts}>casfew</button>
       <div>
-        {/* StageとSectionのモデルの取得に成功。あとは、ステージ毎に横展開して、Sectionを振り分ける。 */}
+        {/* StageとSectionのモデルの取得に成功。あとは、ステージ毎に横展開して、Sectionを振り分ける。! */}
         <TimeTableArea stages={stages} sections={sections}/>
       </div>
     </div>
