ac7cc273d0c47a011b2fbf598c1f9cc23c1f33ee
raisack8 <raisack8@gmail.com>
2023-07-12 23:32:48 +0900

    0712 上手く渡せるようになったけど何故か分からない。。

--- mytiteapi/api/operates/my_tite.py
+++ mytiteapi/api/operates/my_tite.py
@@ -1,5 +1,6 @@
 from ..models import SectionModel,StageModel
 from rest_framework.response import Response
+import datetime
 
 
 class MyTiteGenerator:
@@ -14,15 +15,21 @@ class MyTiteGenerator:
     def post(self, obj, request):
         # POSTリクエストで送信されたデータを取得する
         data = request.data
-        print(data)
         # POSTで送られてきたidに紐づくSectionを返す。
         queryset = SectionModel.objects.filter(id__in=data["id"])
+
+
+        def convert_time_str(start_time):
+            return int(f"{start_time.hour}{'{:02}'.format(start_time.minute)}")
+        
+
         # print(queryset)
         res_list = []
         time_list = []
         for query in queryset:
+            # print(f'{str(query.start_time.hour())}:{str(query.start_time.minute())}')
             obj={
-              'start_time':query.start_time,
+              # 'start_time':convert_time_str(query.start_time),
               'data': {
                 'id':query.id,
                 'fes_id':query.fes_id.id,
@@ -45,16 +52,74 @@ class MyTiteGenerator:
             time_list.append(query.start_time)
             # 選択してきたSectionをリストに格納
             res_list.append(obj)
+            # ★res_listの中身はstart_timeをキーとしてSectionリストができている
         
         # 各Sectionのstart_timeを時間順にソート
         time_list.sort()
-        print(time_list)
+
 
         rtn_list = []
         # 先程ソートした順にsectionの中身を見ていき、時間順でリストに格納する。
         for time in time_list:
             for res in res_list:
-                if res['start_time'] == time:
+                if res['data']['start_time'] == time:
                   rtn_list.append(res['data'])
+                  # 0711 上手くデータを作る方法が思いつかない！！！
 
+
+        continue_flag = True
+        start_time = time_list[0]
+        end_time = time_list[len(time_list)-1]
+        target_time = start_time
+        stage_id = 0
+        return_list = [[],[],[],[]]
+        error_msg = ""
+
+        try:
+          # 自分でもよくわからないアルゴリズム、、
+          while continue_flag:
+              for i,res in enumerate(res_list):
+                  # target_timeと同じresのデータを探す
+                  if res['data']['start_time'] == target_time:
+                      copy_sec = res
+                      res_list.pop(i)
+                      copy_sec['data']['stage'] = stage_id
+                      return_list[stage_id].append(copy_sec)
+                      # 一致したtimeをtime_listから取り除く
+                      for j ,time in enumerate(time_list):
+                          if time == target_time:
+                              time_list.pop(j)
+                              break
+                      # 後ほど5足すので5引いておく、的な？
+                      target_time = target_time + datetime.timedelta(minutes=copy_sec['data']['allotted_time']-5)
+                      continue
+              if len(time_list) == 0:
+                  continue_flag = False
+                  # ループ終了
+                  break
+              
+              if target_time <= end_time:
+                  # 5分繰り上げて再ループ
+                  target_time = target_time + datetime.timedelta(minutes=5)
+                  continue
+              else:
+                  stage_id = stage_id + 1
+                  if stage_id==4:
+                      # インデックスオーバー強制終了
+                      error_msg = "並行して表示できる数を超えています。"
+                      return_list = []
+                  target_time = start_time
+                  continue
+        except Exception as e:
+            error_msg = "システムエラーが発生しました。"
+            # エラーを通知してほしい...
+        # result = {
+        #     'myTiteSections': return_list,
+        #     'errorMsg': error_msg
+        # }
+
+        result = return_list
+        print(return_list)
+            
         return {"message": rtn_list}
+
